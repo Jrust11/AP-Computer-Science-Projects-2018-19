8fe816fd945fc3d58f137179ad749ab7b7d9314d
Jacob Rust <22060477+Jrust11@users.noreply.github.com>
2019-01-15 16:26:32 -0500

    Update ArrayPractice1.java

--- ArrayPractice1.java
+++ ArrayPractice1.java
@@ -1,1 +1,39 @@
-/* Class: Array Practice 1 Author: Jacob Rust Date: September 10, 2018 */import java.awt.*;import BreezyGUI.*;public class ArrayPractice1{		public static void main(String[]args)	{		double[] data = new double[10];				for(int i = 0; i<data.length; i++)		{			data[i] = Math.random();		}				double sum = 0;				for( int i = 0; i<data.length; i++)		{			sum = sum + data[i];		}						System.out.println("Mean = " + sum/data.length);									}									}
+// Name: Jacob Rust
+// Project: Array Practice 1
+ 
+import java.awt.*;
+
+//Breezy GUI is an interface package we use for our Comp Sci class
+//Breezy GUI package will be a package in the master branch
+import BreezyGUI.*;
+
+
+public class ArrayPractice1
+{
+	
+  //Main method
+	public static void main(String[]args)
+	{
+		double[] data = new double[10];
+		
+    //Assigns random doubles to the 10 positions in the array
+		for(int i = 0; i<data.length; i++)
+		{
+			data[i] = Math.random();
+		}
+		
+		double sum = 0;
+		
+    //calculates the sum
+		for( int i = 0; i<data.length; i++)
+		{
+			sum = sum + data[i];
+		}
+		
+		//calculates the mean
+		System.out.println("Mean = " + sum/data.length);
+	
+	}
+	
+	
+}
